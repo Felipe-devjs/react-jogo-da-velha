980a8f3838d6a9836aa6cf6d52d052604d140aa2
Felipe Batista <felipeab2706@gmail.com>
2025-04-07 15:53:14 -0300

    Criado componente de icone

--- Src/components/header/Header.jsx
+++ Src/components/header/Header.jsx
@@ -2,15 +2,20 @@ import styles from "./Header.module.css"
 
 import Title from "../title/Title"
 import Subtitle from "../subTitle/Subtitle"
+import Icon from "../icon/Icon"
 
 function Header () {
     return(
         <div className={styles.header}>
              <Title> Jogo da Velha</Title>
              <Subtitle>Criado por Felipe Batista</Subtitle>
+             <div className= {styles.iconContent}>
+                <Icon iconName="github" link="https://github.com/felipe-devjs" />
+             </div>
         </div>
+        
 
-    )
+    ) 
 }
 
 export default Header
