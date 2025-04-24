17a23e366dbe3fa870284cc037fc7d2a58b4e089
Felipe Batista <felipeab2706@gmail.com>
2025-04-24 16:45:34 -0300

    Criado estados nbo gameOption

--- Src/components/game/Game.jsx
+++ Src/components/game/Game.jsx
@@ -1,18 +1,21 @@
+import { useState } from "react"
 import GameOption from "../gameOption/GameOption"
 import styles from './Game.module.css'
 
 function Game () {
+    const [gameState, setGameState] = useState(Array(9).fill(0))
+    console.log(gameState)
     return (
         <div className={styles.game}>
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
-            <GameOption />
+            {
+                gameState.map((value, pos) => 
+                <GameOption 
+                key={`game-option-pos-${pos}`}
+                status={value}       
+            />
+            )
+            }
+            
         </div>
     )
 }
