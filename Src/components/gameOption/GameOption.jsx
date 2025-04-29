17538b2eaf30107ce237bcd92e1b139fc22bd4cc
Felipe Batista <felipeab2706@gmail.com>
2025-04-29 12:23:00 -0300

    Criado logica do game

--- Src/components/gameOption/GameOption.jsx
+++ Src/components/gameOption/GameOption.jsx
@@ -1,11 +1,11 @@
 import styles from './GameOption.module.css'
 import Icon from '../icon/Icon'
 
-const GameIcon = ({ IconName }) => <Icon iconName={iconName} size="25/px" />
+const GameIcon = ({ iconName }) => <Icon iconName = {iconName} size="25px" />
 
-function GameOption ({ status }){
+function GameOption ({ status, onClick }){
     return (
-        <div className={styles.gameOption}>
+        <div className={styles.gameOption} onClick={onClick}>
             {
                 status === 1 && < GameIcon iconName="circle"  />
             }
