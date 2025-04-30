bd68275d595652b6b6d4d39b88272eaa4d7f887d
Felipe Batista <felipeab2706@gmail.com>
2025-04-30 16:23:03 -0300

    Criado logica para declarar campeao e reset

--- Src/components/game/Game.jsx
+++ Src/components/game/Game.jsx
@@ -2,7 +2,7 @@ import { useState , useEffect } from "react"
 import styles from './Game.module.css'
 
 import GameOption from "../gameOption/GameOption"
-import Icon from "../icon/Icon"
+import GameInfo from "../gameInfo/GameInfo"
 
 const winnerTable = [
     [0,1,2],
@@ -33,9 +33,13 @@ function Game() {
         winnerTable.forEach((line) => {
             const values = line.map((pos) => gameState[pos])
             const sum = values.reduce((sum, value) => sum + value)
-            if (sum === 3 || sum === -3) setWinner(sum / 3)
+            if (sum === 3 || sum === -3) {setWinner(sum / 3)}
         })
+    }
 
+    const handleReset = () => {
+        setGameState(Array(9).fill(0))
+        setWinner(0)
     }
        useEffect(() => {
         setCurrentPlayer(currentPlayer * -1)
@@ -58,15 +62,13 @@ function Game() {
 
             </div>
 
-            <div className= {styles.gameInfo}>
-                <h4>Proximo a jogar:</h4>
-                {
-                    currentPlayer === 1 && <Icon iconName="circle" />
-                }
-                {
-                    currentPlayer === -1 && <Icon iconName="x" />
-                }
-            </div>
+            <GameInfo 
+             currentPlayer={currentPlayer}
+             winner={winner}
+             onReset={handleReset}
+            />
+           
+
         </div>
     )
 }
